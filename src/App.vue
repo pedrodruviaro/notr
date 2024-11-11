a0d64430edb56e112eec1568063d598fac59e393
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-11 18:34:47 -0300

    feat: pages transition

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { useNotesStore } from '@/stores/notes'
 import { onMounted } from 'vue'
 import { useCategoriesStore } from './stores/categories'
+import LayoutPage from '@/layouts/Page.vue'
 
 const notesStore = useNotesStore()
 const categoriesStore = useCategoriesStore()
@@ -13,5 +14,30 @@ onMounted(() => {
 </script>
 
 <template>
-  <RouterView />
+  <div class="overflow-x-hidden">
+    <LayoutPage>
+      <RouterView v-slot="{ Component }">
+        <Transition name="page-transition" mode="out-in">
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
+    </LayoutPage>
+  </div>
 </template>
+
+<style scoped>
+.page-transition-enter-active,
+.page-transition-leave-active {
+  transition: all 0.35s ease;
+}
+
+.page-transition-enter-from {
+  transform: translateX(100%);
+  opacity: 0.5;
+}
+
+.page-transition-leave-to {
+  transform: translateX(-100%);
+  opacity: 1;
+}
+</style>

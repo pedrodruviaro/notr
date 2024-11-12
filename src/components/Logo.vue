0a61cdf62897cb6d4f659cd67b54c667f17b7dc4
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-12 15:58:56 -0300

    refactor: move logo.svg to assets folder

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,6 +2,6 @@
 
 <template>
   <span class="block max-w-max">
-    <img src="/public/logo.svg" alt="Notr" width="107" height="50" />
+    <img src="/src/assets/logo.svg" alt="Notr" width="107" height="50" />
   </span>
 </template>

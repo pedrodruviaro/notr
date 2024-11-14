318270ae01adf57b41a185f5a302fc0512563a50
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-14 17:12:18 -0300

    refactor: improve button component

--- src/components/Base/Button.vue
+++ src/components/Base/Button.vue
@@ -1,73 +1,52 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
+import { computed } from 'vue'
+import { RouterLink } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 
 type Variant = 'solid' | 'ghost' | 'outline'
-
-type VariantClasses = {
-  [key in Variant]: string
-}
 
 const props = withDefaults(
   defineProps<{
     label?: string
-    link?: boolean
-    to?: string | object
+    to?: string | RouteRecordRaw
     block?: boolean
     variant?: Variant
     loading?: boolean
   }>(),
   {
-    to: '/',
     block: false,
     variant: 'solid',
     loading: false,
   },
 )
 
-const variantClasses: VariantClasses = {
+const componentToRender = computed(() => (props.to ? RouterLink : 'button'))
+
+const variantClasses = computed(() => ({
   solid: 'border border-neutral-900 bg-primary hover:brightness-95',
   ghost: 'hover:bg-neutral-100',
   outline: 'border border-neutral-900 hover:bg-neutral-100 hover:border-neutral-700',
-}
+}))
+
+const customButtonClasses = computed(() => [
+  {
+    'w-full': props.block,
+    'max-w-max': !props.block,
+    'cursor-not-allowed opacity-80': props.loading,
+    'cursor-pointer': !props.loading,
+  },
+  variantClasses.value[props.variant],
+])
 </script>
 
 <template>
-  <RouterLink
-    v-if="props.link"
+  <component
+    :is="componentToRender"
     :to="props.to"
-    class="button"
-    :class="[
-      {
-        'w-full': props.block,
-        'max-w-max': !props.block,
-        'cursor-not-allowed opacity-80': props.loading,
-        'cursor-pointer': !props.loading,
-      },
-      variantClasses[props.variant],
-    ]"
+    class="flex items-center justify-center gap-2 rounded-lg px-5 py-1.5 text-center"
+    :class="customButtonClasses"
     :disabled="props.loading"
-  >
-    <template v-if="props.label">
-      {{ props.label }}
-    </template>
-
-    <slot v-else />
-    <Icon icon="mdi:loading" class="animate-spin" v-if="props.loading" />
-  </RouterLink>
-
-  <button
-    class="button"
-    :class="[
-      {
-        'w-full': props.block,
-        'max-w-max': !props.block,
-        'cursor-not-allowed opacity-80': props.loading,
-        'cursor-pointer': !props.loading,
-      },
-      variantClasses[props.variant],
-    ]"
-    :disabled="props.loading"
-    v-else
   >
     <template v-if="props.label">
       {{ props.label }}
@@ -76,11 +55,5 @@ const variantClasses: VariantClasses = {
     <slot v-else />
 
     <Icon icon="mdi:loading" class="animate-spin" v-if="props.loading" />
-  </button>
+  </component>
 </template>
-
-<style scoped>
-.button {
-  @apply flex items-center justify-center gap-2 rounded-lg px-5 py-1.5 text-center;
-}
-</style>
